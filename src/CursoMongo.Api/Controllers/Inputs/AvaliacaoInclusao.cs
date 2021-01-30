1a8536b5e3d0783d64c5e75d5aaa3b2b45619f69
yuriDuque <yurithielmann83@gmail.com>
2021-01-29 22:00:13 -0300

    criando projeto inicial

--- src/CursoMongo.Api/Controllers/Inputs/AvaliacaoInclusao.cs
+++
@@ -1,8 +0,0 @@
-namespace CursoMongo.Api.Controllers.Inputs
-{
-    public class AvaliacaoInclusao
-    {
-        public int Estrelas { get; set; }
-        public string Comentario { get; set; }
-    }
-}
--- src/CursoMongo.Api/Controllers/Inputs/RestauranteAlteracaoParcial.cs
+++
@@ -1,7 +0,0 @@
-namespace CursoMongo.Api.Controllers.Inputs
-{
-    public class RestauranteAlteracaoParcial
-    {
-        public int Cozinha { get; set; }
-    }
-}
