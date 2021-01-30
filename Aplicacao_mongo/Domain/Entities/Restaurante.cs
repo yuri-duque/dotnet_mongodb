2bf9bc9bf295f2766c8cda386f232dbf36bbcd24
yuriDuque <yurithielmann83@gmail.com>
2021-01-29 22:38:50 -0300

    criando validaçoes com fluent validation

--- Aplicacao_mongo/Domain/Entities/Restaurante.cs
+++ Aplicacao_mongo/Domain/Entities/Restaurante.cs
@@ -1,8 +1,11 @@
 ﻿using Domain.Enums;
+using FluentValidation;
+using FluentValidation.Results;
+using System.Linq;
 
 namespace Domain.Entities
 {
-    public class Restaurante
+    public class Restaurante : AbstractValidator<Restaurante>
     {
         public Restaurante(string id, string nome, ECozinha cozinha)
         {
@@ -14,6 +17,46 @@ namespace Domain.Entities
         public string Id { get; }
         public string Nome { get; }
         public ECozinha Cozinha { get; }
-        public Endereco Endereco { get; }
+        public Endereco Endereco { get; private set; }
+
+        #region Validaçoes
+
+        public virtual ValidationResult ValidationResult { get; set; }
+
+        public virtual bool Validar()
+        {
+            ValidarNome();
+            ValidationResult = Validate(this);
+
+            ValidarEndereco();
+
+            return ValidationResult.IsValid;
+        }
+
+        private void ValidarNome()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Nome não pode ser vazio.")
+                .MaximumLength(30).WithMessage("Nome pode ter no maximo 30 caracteres.");
+        }
+
+        private void ValidarEndereco()
+        {
+            if (Endereco.Validar())
+                return;
+
+            ValidationResult.Errors.Union(Endereco.ValidationResult.Errors);
+        }
+
+        #endregion
+
+        #region Declarações
+
+        public void SetEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
+
+        #endregion
     }
 }
--- Aplicacao_mongo/Domain/ValueObjects/Endereco.cs
+++ Aplicacao_mongo/Domain/ValueObjects/Endereco.cs
@@ -1,6 +1,9 @@
-﻿namespace Domain.Entities
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Domain.Entities
 {
-    public class Endereco
+    public class Endereco : AbstractValidator<Endereco>
     {
         public Endereco(string logradouro, string numero, string cidade, string uf, string cep)
         {
@@ -16,5 +19,51 @@
         public string Cidade { get; }
         public string UF { get; }
         public string CEP { get; }
+
+        #region Validaçoes
+
+        public virtual ValidationResult ValidationResult { get; set; }
+
+        public virtual bool Validar()
+        {
+            ValidarLogradouro();
+            ValidarCidade();
+            ValidarUF();
+            ValidarCEP();
+
+            ValidationResult = Validate(this);
+
+            return ValidationResult.IsValid;
+        }
+
+        private void ValidarLogradouro()
+        {
+            RuleFor(c => c.Logradouro)
+                .NotEmpty().WithMessage("Logradouro não pode ser vazio.")
+                .MaximumLength(50).WithMessage("Logradouro pode ter no maximo 50 caracteres.");
+        }
+
+        private void ValidarCidade()
+        {
+            RuleFor(c => c.Logradouro)
+                .NotEmpty().WithMessage("Cidade não pode ser vazio.")
+                .MaximumLength(100).WithMessage("Cidade pode ter no maximo 100 caracteres.");
+        }
+
+        private void ValidarUF()
+        {
+            RuleFor(c => c.Logradouro)
+                .NotEmpty().WithMessage("UF não pode ser vazio.")
+                .MaximumLength(2).WithMessage("UF pode ter no maximo 2 caracteres.");
+        }
+
+        private void ValidarCEP()
+        {
+            RuleFor(c => c.Logradouro)
+                .NotEmpty().WithMessage("CEP não pode ser vazio.")
+                .MaximumLength(8).WithMessage("CEP pode ter no maximo 8 caracteres.");
+        }
+
+        #endregion
     }
 }
