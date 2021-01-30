e3f52bb879adad1ceaddf8c3b94ae07a80db6ef7
yuriDuque <yurithielmann83@gmail.com>
2021-01-29 22:16:35 -0300

    criando entidade restaurante

---
+++ Aplicacao_mongo/Domain/Entities/Restaurante.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Restaurante
+    {
+        public Restaurante(string id, string nome, ECozinha cozinha)
+        {
+            Id = id;
+            Nome = nome;
+            Cozinha = cozinha;
+        }
+
+        public string Id { get; }
+        public string Nome { get; }
+        public ECozinha Cozinha { get; }
+        public Endereco Endereco { get; }
+    }
+}
