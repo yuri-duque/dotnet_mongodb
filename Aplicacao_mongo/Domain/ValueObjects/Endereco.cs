5a543ee922258fe6b84124af21a85d5184b8a54a
yuriDuque <yurithielmann83@gmail.com>
2021-01-29 22:17:05 -0300

    criando objeto de valor endereco

---
+++ Aplicacao_mongo/Domain/ValueObjects/Endereco.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Entities
+{
+    public class Endereco
+    {
+        public Endereco(string logradouro, string numero, string cidade, string uf, string cep)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Cidade = cidade;
+            UF = uf;
+            CEP = cep;
+        }
+
+        public string Logradouro { get; }
+        public string Numero { get; }
+        public string Cidade { get; }
+        public string UF { get; }
+        public string CEP { get; }
+    }
+}
